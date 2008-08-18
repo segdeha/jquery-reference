44d25b8d75d0f9811ea77f90a5236aa120fed8e2
segdeha <segdeha@28792181-9f54-0410-ac0a-a522d75e7ed1>
2008-08-18 10:46:08 +0000

    Got docs update script working. Added stubs for version checking.

--- trunk/jQueryReference.wdgt/Assets/js/docs.js
+++ trunk/jQueryReference.wdgt/Assets/js/docs.js
@@ -4,13 +4,19 @@
 	2008-08-17
 */
 
-DOCS = (function () {
+var DOCS = (function () {
 	// private members
-	var _urls;
+	var _urls, _callback;
 	_urls = {
-		update : 'http://jquery-api-browser.googlecode.com/svn/trunk/api-docs.xml',
 		xml    : MAIN.base + '/Assets/xml/api-docs.xml',
 		xsl    : MAIN.base + '/Assets/xsl/main.xsl'
+	};
+	_callback = function (obj) {
+		if (obj.outputString.indexOf('SUCCESS') > -1) {
+			// success
+		} else {
+			// error
+		}
 	};
 	// public members
 	return {
@@ -18,7 +24,7 @@ DOCS = (function () {
 			$('#front .content').xslt(_urls.xml, _urls.xsl);
 		},
 		update: function () {
-			
+			WW.system(MAIN.base + '/Assets/php/update-docs.php', _callback);
 		}
 	};
 })();
--- trunk/jQueryReference.wdgt/Assets/php/update-docs.php
+++ trunk/jQueryReference.wdgt/Assets/php/update-docs.php
@@ -7,42 +7,32 @@
 // might need this if there are redirects going on: CURLOPT_FOLLOWLOCATION
 // if we use the above, we may need to set CURLOPT_MAXREDIRS
 
-// add stylesheet declaration as 2nd line
-// <?xml-stylesheet type="text/xsl" href="your_transform_file_here.xsl" ?>
+$dst = substr(__FILE__, 0, -19) . 'xml/api-docs.xml';
+$url = 'http://jquery-api-browser.googlecode.com/svn/trunk/api-docs.xml';
 
-$dir   = substr(__FILE__, 0, -22) . 'php_manual/';
-$tar   = 'tar --directory %s -xzf ';
+// move old docs
+exec('mv ' . $dst . ' ' . $dst . '.bak');
 
-if (isset($argv[1])) {
-	$cc  = $argv[1]; // e.g., pt_BR or en
-	$fnm = 'php_manual_' . $cc. '.tar.gz';
-	$url = 'http://www.php.net/get/' . $fnm . '/from/www.php.net/mirror';
-	$dst = $dir . $fnm;
-	// get the file via curl
-	$ch = curl_init($url);
-	$fp = fopen($dst, 'w');
-	curl_setopt($ch, CURLOPT_FILE,           $fp);
-	curl_setopt($ch, CURLOPT_MUTE,           1);
-	curl_setopt($ch, CURLOPT_HEADER,         0);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_USERAGENT,      'PHP Function Reference Widget 1.0 (andrew.hedges.name/widgets)');
-	$result = curl_exec($ch);
-	curl_close($ch);
-	fclose($fp);
-	// uncompress it
-	@mkdir($dir . $cc);
-	exec('mv ' . $dir . $fnm . ' ' . $dir . $cc . '/' . $fnm);
-	// insert lang code into $cmd
-	$cmd = sprintf($tar, $dir . $cc) . $dir . $cc . '/' . $fnm;
-	exec($cmd);
-	if ($result) {
-		$return = 'SUCCESS: Language downloaded and uncompressed successfully';
-	} else {
-		$return = 'ERROR: There was a problem downloading the file.';
-	}
+// get the file via curl
+$ch = curl_init($url);
+$fp = fopen($dst, 'w');
+curl_setopt($ch, CURLOPT_FILE,           $fp);
+//curl_setopt($ch, CURLOPT_MUTE,           1);
+curl_setopt($ch, CURLOPT_MUTE,           0);
+curl_setopt($ch, CURLOPT_HEADER,         0);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+curl_setopt($ch, CURLOPT_USERAGENT,      'jQuery Reference Widget 1.0 (andrew.hedges.name/widgets)');
+$result = curl_exec($ch);
+curl_close($ch);
+fclose($fp);
+
+if ($result) {
+	$return = 'SUCCESS: XML downloaded and installed successfully';
 } else {
-	$return = 'ERROR: No language specified';
+	$return = 'ERROR: There was a problem downloading the file.';
+	// move old docs back
+	exec('mv ' . $dst . '.bak' . ' ' . $dst);
 }
 
 fwrite(STDOUT, $return);
