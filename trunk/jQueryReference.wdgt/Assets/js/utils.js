6e906071f693948ba212a3abf0078110763c9c8e
segdeha <segdeha@28792181-9f54-0410-ac0a-a522d75e7ed1>
2008-09-02 10:43:54 +0000

    The selectors section now displays better. Other minor changes.

--- trunk/jQueryReference.wdgt/Assets/js/utils.js
+++ trunk/jQueryReference.wdgt/Assets/js/utils.js
@@ -7,14 +7,35 @@
 var UTILS = (function () {
 	// private members
 	var
+		_rgxps,
 		_paypal
 	;
+	
+	// regular expressions
+	_rgxps = {
+		elipsis : /\.{3}\s/g,
+		quotes  : {
+			open  : /\s\"/g,
+			close : /"\s/g
+		}
+	};
 	
 	// Link to PayPal donations page
 	_paypal = 'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=andrew%40hedges%2ename&item_name=jQuery%20Reference%20Widget&amount=5%2e00&no_shipping=1&cn=Say%20%27hi%27%20to%20the%20developer%21&tax=0&currency_code=EUR&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8';
 	
 	// public members
 	return {
+		/*	Do some simple text transformations for smart quotes and elipses
+			@param string str String to be educated
+			@return string
+		*/
+		sortaSmarty: function (str) {
+			return str
+				.replace(_rgxps.elipsis, '&#8230;')
+				.replace(_rgxps.quotes.open, ' &#8220;')
+				.replace(_rgxps.quotes.close, '&#8221; ')
+			;
+		},
 		/*	Localize a string (based on a function in Apple's Dashcode)
 			@param string str
 			@return string
@@ -56,4 +77,9 @@ var __ = function (str) {
 // Shortcut to UTILS.go
 var _go = function (url) {
 	UTILS.gotoUrl(url);
+};
+
+// Shortcut to UTILS.sortaSmarty
+var _ed = function (str) {
+	return UTILS.sortaSmarty(str);
 };
